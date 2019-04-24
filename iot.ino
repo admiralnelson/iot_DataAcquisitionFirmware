562f7d3d668a4e2e7017e9cf95f30e5adb29ce7d
horatio nelson <admiralofinternetmeme@outlook.com>
2019-04-24 18:04:56 +0700

    dht

--- iot.ino
+++ iot.ino
@@ -4,22 +4,52 @@
  Author:	MIIX 510
 */
 // the setup function runs once when you press reset or power the board
+#include <DHT_U.h>
+#include <DHT.h>
 #include <WiFi.h>
 #include <Arduino_JSON.h>
 #include <HTTPClient.h>
 
 
+
 #include <dummy.h>
 
 #define URL_UPDATE "http://35.240.207.36/"
+#define URL_UPDATE "http://35.240.207.36/Device/PostLog"
 #define WIFI_SSID "Firhan Wibianto"
 #define WIFI_PASS "jarangpulang"
+
+#define DHT_PORT 0
+#define DHT_TYPE 11
 
 bool isInternetOK()
 {
 	HTTPClient http;
-	http.begin("example.com");
+	http.begin("http://35.240.207.36/");
 	int code = http.GET();
+	http.end();
+	return (code == HTTP_CODE_OK);
+
+}
+
+bool PostData()
+{
+	HTTPClient http;
+	DHT dht(DHT_PORT, DHT_TYPE);
+	JSONVar json;
+
+	dht.begin();
+
+	json["DeviceId"] = 1;
+	json["Temperature"] = dht.readTemperature();
+	json["Humidity"] = dht.readHumidity();
+	json["Sound"] = 0;
+	
+	http.begin("http://35.240.207.36/Device/PostLog");
+	http.addHeader("Content-Type", "application/json; charset=utf-8");
+	int code = http.POST(JSON.stringify(json));
+	http.end();
+	Serial.println(http.getString());
 
 	return (code == HTTP_CODE_OK);
 }
@@ -34,6 +64,7 @@ void setup()
 	int i = 0;
 	while (WiFi.status() != WL_CONNECTED && i < 40)
 	{
+		delay(500);
 		Serial.write(".");
 		i++;
 	}
@@ -49,5 +80,9 @@ void setup()
 // the loop function runs over and over again until power down or reset
 void loop() 
 {
-  
+	while (true)
+	{
+		delay(5000);
+		PostData();
+	}
 }
